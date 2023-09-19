9bc2d29612b53ed5874608e8937c81ea969d916a
Himanshu <100996941+Himanshuu03@users.noreply.github.com>
2023-09-19 12:16:17 +0530

    Create maximunAvgSubArray.cpp

---
+++ week1(Array)/maximunAvgSubArray.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+    double burteForce(vector<int>& nums, int k){
+        int maxSum = INT_MIN;
+        int j = k-1;
+        int start = 0;
+        while(j < nums.size()){
+            int sum = 0;
+            for(int i=start;i <= j;i++){
+                sum = sum +nums[i];
+            }
+            maxSum = max(sum,maxSum); 
+            start++;
+            j++;
+        }
+        double maxAvg = maxSum /(double)k;
+        return maxAvg;
+    }
+
+    double optimalSol(vector<int>& nums, int k){
+        int maxSum = INT_MIN;
+        int j = k-1;
+        int start = 0;
+        int sum = 0;
+        for(int i=start;i<=j;i++){
+            sum = sum +nums[i];
+        }
+        maxSum = max(sum,maxSum);
+        j++;
+        while(j<nums.size()){
+            sum = sum - nums[start++] + nums[j];
+            maxSum = max(sum,maxSum); 
+            j++;
+        }
+        double maxAvg = maxSum / (double)k;
+        return maxAvg;
+    }
+    double findMaxAverage(vector<int>& nums, int k) {
+        // return burteForce(nums,k);
+        return optimalSol(nums,k);
+    }
+};
