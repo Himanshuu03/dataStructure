4f015c568e1ae9100795d3893bcb1674e0088a8e
Himanshu <100996941+Himanshuu03@users.noreply.github.com>
2023-09-19 09:09:34 +0530

    Update missingNumber.cpp

--- week1(Array)/missingNumber.cpp
+++ week1(Array)/missingNumber.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int arrSum = 0;
-        for(int i=0;i<nums.size();i++){
-            arrSum = arrSum+nums[i];
+        // int arrSum = 0;
+        // for(int i=0;i<nums.size();i++){
+        //     arrSum = arrSum+nums[i];
+        // }
+        // return (((nums.size()+1)*nums.size())/2)-arrSum;
+
+        
+        //another soln
+        int ans = 0;
+        for(int i =0;i<nums.size();i++){
+            ans = ans^nums[i]^i;
         }
-        return (((nums.size()+1)*nums.size())/2)-arrSum;
+        return ans^nums.size();
     }
 };
