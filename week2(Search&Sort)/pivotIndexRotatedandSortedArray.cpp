11f69fb49ae670ee3edd5d95ac3f6e84d6f30d08
Himanshu <100996941+Himanshuu03@users.noreply.github.com>
2023-09-22 08:14:04 +0530

    Update pivotIndexRotatedandSortedArray.cpp
    <= , mid+1 ,mid -1

--- week2(Search&Sort)/pivotIndexRotatedandSortedArray.cpp
+++ week2(Search&Sort)/pivotIndexRotatedandSortedArray.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
-int pivotElement(vector<int>arr){
+int findPivot(vector<int>arr){
     int start = 0;
     int end = arr.size()-1;
     int mid = start + (end-start)/2;
@@ -10,25 +11,29 @@ int pivotElement(vector<int>arr){
         if(start == end){
             return start;
         }
-        if(arr[mid] > arr[mid+1]){
+        else if(mid+1 < arr.size() && arr[mid]>arr[mid+1]){
             return mid;
         }
-        else if(arr[mid] < arr[mid-1]){
+        else if(mid-1 >= 0 && arr[mid-1]>arr[mid]){
             return mid-1;
         }
         else if(arr[start] > arr[mid]){
             end = mid-1;
         }
-        else{
+        
+        //point to remenber <= because ther can be a duplicate
+        
+        else if (arr[start] <= arr[mid]){
             start = mid+1;
         }
-        mid = start + (end-start)/2;
+        mid = start +(end-start)/2;
     }
-    return -1;
+    return 0;
 }
 
 int main()
 {
-    vector<int>arr{12,14,16,2,4,6,8,10};
-    cout<<pivotElement(arr);
+    vector<int>arr{4,5,6,7,0,1,2};
+    cout<<findPivot(arr);
+    return 0;
 }
