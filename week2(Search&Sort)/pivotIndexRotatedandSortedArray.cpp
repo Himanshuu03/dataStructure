b464a19813b175a70f059d2f45fbc1f67439d3e4
Himanshu <100996941+Himanshuu03@users.noreply.github.com>
2023-09-22 08:25:23 +0530

    Update pivotIndexRotatedandSortedArray.cpp

--- week2(Search&Sort)/pivotIndexRotatedandSortedArray.cpp
+++ week2(Search&Sort)/pivotIndexRotatedandSortedArray.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-
+#include <math.h>
 using namespace std;
 
 int findPivot(vector<int>arr){
@@ -31,9 +31,47 @@ int findPivot(vector<int>arr){
     return 0;
 }
 
+int findPivot2(vector<int>arr){
+    int start = 0;
+    int end = arr.size()-1;
+    int mid = start + (end-start)/2;
+    int ans = 0;
+    while(start<=end){
+        if(arr[start] <= arr[mid]){
+            ans = max(ans,arr[mid]);
+            start = mid+1;
+        }
+        else if(arr[start] > arr[mid]){
+            ans = max(ans,arr[mid]);
+            end = mid-1;
+        }
+        mid = start +(end-start)/2;
+    }
+    return ans;
+}
+
+int binarySearch(vector<int>arr,int target){
+    int start = 0;
+    int end = arr.size()-1;
+    int mid = start +(end-start)/2;
+    while(start <= end){
+        if(arr[mid] == target){
+            return mid;
+        }
+        else if(target > arr[mid]){
+            start = mid+1;
+        }
+        else{
+            end = mid-1;
+        }
+        mid = start +(end-start)/2;
+    }
+    return -1;
+}
+
 int main()
 {
     vector<int>arr{4,5,6,7,0,1,2};
-    cout<<findPivot(arr);
+    cout<<binarySearch(arr,findPivot2(arr));
     return 0;
 }
